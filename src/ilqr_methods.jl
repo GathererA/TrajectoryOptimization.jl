8750a77525a46f752cfaf2fa887404911a6ceed7
Andrew Gatherer <gatherer@stanford.edu>
2019-01-05 16:44:56 -0800

    updated cost function

--- src/ilqr_methods.jl
+++ src/ilqr_methods.jl
@@ -218,7 +218,7 @@ function stage_cost(x,u,Q::AbstractArray{Float64,2},R::AbstractArray{Float64,2},
 end
 
 function stage_cost_sat_att(x,u,Q::AbstractArray{Float64,2},R::AbstractArray{Float64,2},xf::Vector{Float64},c::Float64=0)::Union{Float64,ForwardDiff.Dual}
-    0.5*(x[1:9] - xf[1:9])'*Q[1:9,1:9]*(x[1:9] - xf[1:9]) + 0.5*u'*R*u + norm(Q[10:13,10:13])*(1-(x[10:13]'*xf[10:13])/norm(x[10:13]))
+    0.5*(x[1:9] - xf[1:9])'*Q[1:9,1:9]*(x[1:9] - xf[1:9]) + 0.5*u'*R*u + 0.5*norm(Q[10:13,10:13])*(1-(x[10:13]'*xf[10:13])/norm(x[10:13]))
 end
 
 function stage_cost(obj::Objective, x::Vector, u::Vector)::Float64
